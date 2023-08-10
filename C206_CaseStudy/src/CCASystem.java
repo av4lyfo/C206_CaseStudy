c1d7f60410dd1b864207638c6ea89a1249a345fb
YK-36 <22015127@rp.edu.sg>
2023-08-10 16:35:05 +0800

    fix

--- C206_CaseStudy/src/CCASystem.java
+++ C206_CaseStudy/src/CCASystem.java
@@ -209,10 +209,12 @@ public class CCASystem {
 		System.out.println("1. Add Activity");
 		System.out.println("2. View Activities");
 		System.out.println("3. Delete Activity");
-		System.out.println("4. View Registrations");
-		System.out.println("5. Manage Registration Approval");
-		System.out.println("6. Edit Attendance");
-		System.out.println("7. View Attendance");
+		System.out.println("4. Add Approval Status");
+		System.out.println("5. View Approval Status");
+		System.out.println("6. Delete Approval Status");
+		System.out.println("7. Add Attendance");
+		System.out.println("8. View Attendance");
+		System.out.println("9. Delete Attendance");
 		System.out.println("0. Exit");
 		System.out.print("Enter your choice: ");
 	}
